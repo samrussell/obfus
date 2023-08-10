6e8880348ecdae52e2c9c458edb9c89385b3ee69
Sam Russell <sam.h.russell@gmail.com>
2023-08-10 20:53:45 +0200

    Attempt at cff but cannot add successor correctly

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,7 +2,9 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/ModuleSlotTracker.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IRBuilder.h"
 #include <iostream>
 
 using namespace llvm;
@@ -30,7 +32,69 @@ namespace obfs {
                 outs() << " Instruction: " << instruction << "\n";
               }
             }
-            return PreservedAnalyses::all();
+
+            // let's try rerouting some blocks?
+
+            BasicBlock &EntryBlock = F.getEntryBlock();
+
+            // thanks Romain!
+            SmallVector<BasicBlock*, 20> flattenedBB;
+
+            for (BasicBlock& BB : F) {
+              if (&BB == &EntryBlock) {
+                outs() << "Not flattening entry block " << getSimpleNodeLabel(&BB) << "\n";
+                continue;
+              }
+              outs() << "Adding block to flatten: " << getSimpleNodeLabel(&BB) << "\n";
+              flattenedBB.push_back(&BB);
+            }
+
+            // add our new var
+
+            IRBuilder<> EntryBuilder(&EntryBlock, EntryBlock.begin());
+            Type *Int32Ty = IntegerType::get(F.getContext(), 32);
+            AllocaInst *Counter = EntryBuilder.CreateAlloca(Int32Ty, nullptr, "counter");
+            EntryBuilder.CreateStore(ConstantInt::get(Int32Ty, 0), Counter);
+
+            // we'll add our dispatch block after the entry block
+            //BasicBlock* DispatchBlock = 
+
+            for (BasicBlock* pToflatBB : flattenedBB){
+              // only handle branches for now
+              outs() << "Flattening block " << getSimpleNodeLabel(pToflatBB) << "\n";
+              if (auto* br = dyn_cast<BranchInst>(pToflatBB->getTerminator())) {
+                // we'll just print them out
+                for (unsigned i = 0; i < br->getNumSuccessors(); ++i) {
+                  BasicBlock *Successor = br->getSuccessor(i);
+                  outs() << "Successor: " << getSimpleNodeLabel(Successor) << "\n";
+                  // ok here we can add a block in the middle
+                  BasicBlock *NewBlock = BasicBlock::Create(F.getContext(), "", &F, pToflatBB);
+
+                  // Increment the counter in the new block
+                  IRBuilder<> Builder(NewBlock);
+                  LoadInst *LoadedCounter = Builder.CreateLoad(Int32Ty, Counter);
+                  Value *IncrementedCounter = Builder.CreateAdd(LoadedCounter, ConstantInt::get(Int32Ty, 1));
+                  Builder.CreateStore(IncrementedCounter, Counter);
+                  Builder.CreateBr(Successor);
+
+                  // Modify the existing terminator to point to the new block
+                  br->setSuccessor(i, NewBlock);
+                }
+              }
+            }
+
+            // print again to see what changed
+
+            outs() << "After flattening:\n";
+            
+            for (BasicBlock& BB : F) {
+              outs() << "New basic block " << getSimpleNodeLabel(&BB) << "\n";
+              for(Instruction& instruction : BB) {
+                outs() << " Instruction: " << instruction << "\n";
+              }
+            }
+
+            return PreservedAnalyses::none();
         }
     };
 }
