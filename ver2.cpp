f1a27e4c76280b4af2b030252ddfa7060eb78f9c
Sam Russell <sam.h.russell@gmail.com>
2023-08-16 21:39:50 +0200

    Working, checkpoint before redoing dispatch

--- ver2.cpp
+++ ver2.cpp
@@ -68,8 +68,6 @@ namespace obfs {
                 // SR insert old code
                 // let's try rerouting some blocks?
 
-                BasicBlock &EntryBlock = F.getEntryBlock();
-
                 // thanks Romain!
                 SmallVector<BasicBlock*, 20> flattenedBB;
                 // demote PHI nodes
@@ -77,49 +75,78 @@ namespace obfs {
                 demotePhiNodes(F);
 
                 for (BasicBlock& BB : F) {
-                // if (&BB == &EntryBlock) {
-                //   outs() << "Not flattening entry block " << getSimpleNodeLabel(&BB) << "\n";
-                //   continue;
-                // }
-                outs() << "Adding block to flatten: " << getSimpleNodeLabel(&BB) << "\n";
-                flattenedBB.push_back(&BB);
+                    if (&BB == &(F.getEntryBlock())) {
+                        outs() << "Not flattening entry block " << getSimpleNodeLabel(&BB) << "\n";
+                        continue;
+                    }
+                    outs() << "Adding block to flatten: " << getSimpleNodeLabel(&BB) << "\n";
+                    flattenedBB.push_back(&BB);
                 }
 
                 // add our new var
 
-                IRBuilder<> EntryBuilder(&EntryBlock, EntryBlock.begin());
+                IRBuilder<> EntryBuilder(&(F.getEntryBlock()), F.getEntryBlock().begin());
                 Type *Int32Ty = IntegerType::get(F.getContext(), 32);
                 AllocaInst *Counter = EntryBuilder.CreateAlloca(Int32Ty, nullptr, "counter");
                 EntryBuilder.CreateStore(ConstantInt::get(Int32Ty, 0), Counter);
 
+                // SR phase 2: add dispatch
+                // split first block so we can treat it like any other
+                BasicBlock &tempEntryBlock = F.getEntryBlock();
+                BasicBlock* pNewEntryBlock = tempEntryBlock.splitBasicBlockBefore(tempEntryBlock.getTerminator(), "");
+                // old EntryBlock points to the new one (?!)
+                // add this to the list to flatten
+                flattenedBB.push_back(&tempEntryBlock);
+
                 // we'll add our dispatch block after the entry block
-                //BasicBlock* DispatchBlock = 
+                BasicBlock* DispatchBlock = BasicBlock::Create(F.getContext(), "dispatch_block", &F);
+
+                // get the successort from the new entry block
+                auto* br = dyn_cast<BranchInst>(pNewEntryBlock->getTerminator());
+                BasicBlock *Successor = br->getSuccessor(0);
+
+                IRBuilder<> EntryBuilder2(DispatchBlock, DispatchBlock->begin());
+                Type *Int32Ty2 = IntegerType::get(F.getContext(), 32);
+                AllocaInst *DispatchVar = EntryBuilder2.CreateAlloca(Int32Ty2, nullptr, "dispatch_var");
+                EntryBuilder2.CreateStore(ConstantInt::get(Int32Ty2, 0x1001), DispatchVar);
+                EntryBuilder2.CreateBr(Successor);
+
+                // Modify the existing terminator to point to the new block
+                // we just split this so we can assume it's an unconditional branch
+                // should probably assert here?
+                br->setSuccessor(0, DispatchBlock);
+                
+                // move after our block
+                DispatchBlock->moveAfter(pNewEntryBlock);
+
+
+                // SR phase 2 end
 
                 for (BasicBlock* pToflatBB : flattenedBB){
-                // only handle branches for now
-                outs() << "Flattening block " << getSimpleNodeLabel(pToflatBB) << "\n";
-                if (auto* br = dyn_cast<BranchInst>(pToflatBB->getTerminator())) {
-                    // we'll just print them out
-                    for (unsigned i = 0; i < br->getNumSuccessors(); ++i) {
-                    BasicBlock *Successor = br->getSuccessor(i);
-                    outs() << "Successor: " << getSimpleNodeLabel(Successor) << "\n";
-                    // ok here we can add a block in the middle
-                    BasicBlock *NewBlock = BasicBlock::Create(F.getContext(), "", &F);
+                    // only handle branches for now
+                    outs() << "Flattening block " << getSimpleNodeLabel(pToflatBB) << "\n";
+                    if (auto* br = dyn_cast<BranchInst>(pToflatBB->getTerminator())) {
+                        // we'll just print them out
+                        for (unsigned i = 0; i < br->getNumSuccessors(); ++i) {
+                        BasicBlock *Successor = br->getSuccessor(i);
+                        outs() << "Successor: " << getSimpleNodeLabel(Successor) << "\n";
+                        // ok here we can add a block in the middle
+                        BasicBlock *NewBlock = BasicBlock::Create(F.getContext(), "", &F);
 
-                    // Increment the counter in the new block
-                    IRBuilder<> Builder(NewBlock);
-                    LoadInst *LoadedCounter = Builder.CreateLoad(Int32Ty, Counter);
-                    Value *IncrementedCounter = Builder.CreateAdd(LoadedCounter, ConstantInt::get(Int32Ty, 1));
-                    Builder.CreateStore(IncrementedCounter, Counter);
-                    Builder.CreateBr(Successor);
+                        // Increment the counter in the new block
+                        IRBuilder<> Builder(NewBlock);
+                        LoadInst *LoadedCounter = Builder.CreateLoad(Int32Ty, Counter);
+                        Value *IncrementedCounter = Builder.CreateAdd(LoadedCounter, ConstantInt::get(Int32Ty, 1));
+                        Builder.CreateStore(IncrementedCounter, Counter);
+                        Builder.CreateBr(Successor);
 
-                    // Modify the existing terminator to point to the new block
-                    br->setSuccessor(i, NewBlock);
-                    
-                    // move after our block
-                    NewBlock->moveAfter(pToflatBB);
+                        // Modify the existing terminator to point to the new block
+                        br->setSuccessor(i, NewBlock);
+                        
+                        // move after our block
+                        NewBlock->moveAfter(pToflatBB);
+                        }
                     }
-                }
                 }
 
                 // print again to see what changed
@@ -127,10 +154,10 @@ namespace obfs {
                 outs() << "After flattening:\n";
                 
                 for (BasicBlock& BB : F) {
-                outs() << "New basic block " << getSimpleNodeLabel(&BB) << "\n";
-                for(Instruction& instruction : BB) {
-                    outs() << " Instruction: " << instruction << "\n";
-                }
+                    outs() << "New basic block " << getSimpleNodeLabel(&BB) << "\n";
+                    for(Instruction& instruction : BB) {
+                        outs() << " Instruction: " << instruction << "\n";
+                    }
                 }
 
                 // OMVLL does a reg2mem() call here
